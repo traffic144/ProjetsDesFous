6334a4306eda509ab0c053ce4ee299c82fe89829
traffic144 <traffic144@gmail.com>
2014-04-30 13:58:22 +0200

    remote commit

--- Foo/src/Test.java
+++ Foo/src/Test.java
@@ -4,7 +4,7 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("foo stands for f*cked up.");
 		System.out.println("foo stands for f*cked up!");
-
+		System.out.println("foo stands for f*cked up!!!");
 	}
 
 }
